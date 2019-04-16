38bbb10281690255dd0a75d87e2e47786dd35048
Nrin <Nrin3141@users.noreply.github.com>
2019-04-16 20:09:31 +0200

    Change paths

--- createDifferentImageSizes.js
+++ createDifferentImageSizes.js
@@ -1,20 +1,26 @@
 const fs = require("fs");
 const sharp = require("sharp");
+const path = require("path");
 
-fs.readdir("./static/gallery", (err, data) => {
+fs.readdir("/static/gallery", (err, data) => {
   data.forEach(image => {
     const widths = [400, 800, 1600, 2400];
     widths.forEach(width => {
-      generateImage(width, image);
+      generateMissingDirectories(width, image)
+        .then(() => {
+          sharp(`/static/gallery/${image}`)
+            .resize({ width })
+            .toFile(`/static/sizes/${width}/${image}`)
+            .then(() => {});
+          return;
+        })
+        .catch(err => console.error(err));
     });
   });
 });
-async function generateImage(width, image) {
-  await fs.mkdir(`static/sizes/${width}`, { recursive: true }, err => {
+async function generateMissingDirectories(width, image) {
+  await fs.mkdir(`/static/sizes/${width}`, { recursive: true }, err => {
     if (err) throw err;
-    sharp(`static/gallery/${image}`)
-      .resize({ width })
-      .toFile(`static/sizes/${width}/${image}`)
-      .then(() => {});
   });
+  return;
 }
