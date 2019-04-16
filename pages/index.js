5dd26246f747a0315e554374991682d803099a09
Nrin <Nrin3141@users.noreply.github.com>
2019-04-16 17:18:56 +0200

    Add test message to homepage

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 
 const Home = () => (
   <div>
-    <Layout content={<p>This is the home page</p>} />
+    <Layout content={<p>This is the home page - and this is a test!</p>} />
   </div>
 );
 export default Home;
