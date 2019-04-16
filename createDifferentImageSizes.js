2779672b15607fa370805051bf7ddfcf9c880b59
Nrin <Nrin3141@users.noreply.github.com>
2019-04-16 13:54:09 +0200

    Remove static sizes from git repo
    Static sizes can be generated from the static/gallery automatically
    after npm installing on a new machine by running node
    createDifferentImageSizes.js

--- createDifferentImageSizes.js
+++ createDifferentImageSizes.js
@@ -5,13 +5,16 @@ fs.readdir("./static/gallery", (err, data) => {
   data.forEach(image => {
     const widths = [400, 800, 1600, 2400];
     widths.forEach(width => {
-      fs.mkdir(`./static/sizes/${width}`, { recursive: true }, err => {
-        if (err) throw err;
-      });
-      sharp(`./static/gallery/${image}`)
-        .resize({ width })
-        .toFile(`./static/sizes/${width}/${image}`)
-        .then(() => {});
+      generateImage(width, image);
     });
   });
 });
+async function generateImage(width, image) {
+  await fs.mkdir(`./static/sizes/${width}`, { recursive: true }, err => {
+    if (err) throw err;
+    sharp(`./static/gallery/${image}`)
+      .resize({ width })
+      .toFile(`./static/sizes/${width}/${image}`)
+      .then(() => {});
+  });
+}
