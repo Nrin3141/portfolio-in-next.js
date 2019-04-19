b807cdeaa9e3f08af8f74370fa3eb6299426e2aa
Nrin <Nrin3141@users.noreply.github.com>
2019-04-19 11:44:07 +0200

    Add preload functionality

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a216631f74ccb525f1f7";
+/******/ 	var hotCurrentHash = "214a1eaacafa0b94f906";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
