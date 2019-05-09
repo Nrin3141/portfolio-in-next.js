e37308b323669e07271d27d7dda9b24abfbd294c
Nrin <Nrin3141@users.noreply.github.com>
2019-05-09 12:36:51 +0200

    Add flex-wrapping to homepage h1

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cf42b5364d8234ccbb71";
+/******/ 	var hotCurrentHash = "fffb7357198e24ce8919";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
